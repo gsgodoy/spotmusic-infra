b706afb8c3451da97a08a305b1ca902fa42702f1
gsgodoy <gustavosouzagodoy@hotmail.com>
2022-07-02 15:11:19 -0300

    Add Providers

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,5 +1,41 @@
 # Cria uma VM no Google Cloud
 
+resource "google_artifact_registry_repository" "my-repo" {
+  provider = google-beta
+
+  location = "us-east4"
+  repository_id = "spotmusic-back"
+  description = "Imagens Docker"
+  format = "DOCKER"
+}
+
+variable "database_password" {
+    type = string
+}
+variable "database_user" {
+    type = string
+}
+
+resource "google_sql_database_instance" "instance" {
+    name="spotmusic"
+    region="us-east4"
+    database_version="MYSQL_8_0"
+    deletion_protection = false
+    settings{
+        tier="db-f1-micro"
+    }
+}
+resource "google_sql_database" "database"{
+    name="playlist"
+    instance=google_sql_database_instance.instance.name
+}
+resource "google_sql_user" "database-user" {
+    name = var.database_user
+    instance = google_sql_database_instance.instance.name
+    password = var.database_password
+}
+# Cria uma VM no Google Cloud
+
 resource "google_app_engine_application" "app" {
   project     = "hackathon-7aso-grupo-04"
   location_id = "us-central"
--- providers.tf
+++ providers.tf
@@ -10,6 +10,28 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
+  project = "hackaaso"
+  region  = "us-east4"
+  zone    = "us-east4-c"
+}
+
+provider "google-beta" {
+  project = "hackaaso"
+  region  = "us-east4"
+  zone    = "us-east4-c"
+}
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.27.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
   project = "hackathon-7aso-grupo-04"
   region  = "us-west1"
   zone    = "us-west1-b"
